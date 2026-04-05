3525a3a22a69798e6ccb3497c42f6b35a8f0ef4d
TheVarius <146878091+TheVarius@users.noreply.github.com>
2026-04-05 15:20:24 +0200

    Add Solution class to calculate maximum wealth

---
+++ Easy/RichestCustomerWealth.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        max = 0
+        for i in accounts:
+            if sum(i) > max:
+                max = sum(i)
+        return max
+
+
+        
