67dcc20fa039a32c7ab3d9cf448e5352a0541fd8
TheVarius <146878091+TheVarius@users.noreply.github.com>
2026-04-05 15:27:31 +0200

    Implement findClosest method to determine nearest person

---
+++ Easy/FindClosestPerson.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        if abs(z-x) > abs(z-y):
+            return 2
+        elif abs(z-x) < abs(z-y):
+            return 1
+        elif abs(z-x) == abs(z-y):
+            return 0
+
+        
